e2fbf75a734b23cbccb7fc4bbb83a234c30a71c6
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-04-14 23:36:04 +0200

    Chap 11 - Binding data to the View

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,19 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page padding>
+    <h5>{{ message }}</h5>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  data() {
+    return {
+      message: 'I love Vue.JS !!'
+    }
+  }
 }
 </script>
+
+<style>
+
+</style>
