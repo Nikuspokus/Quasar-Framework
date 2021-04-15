9f59233cbbd50302de63b73c85fb3054defbbff6
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-04-15 11:35:28 +0200

    Chap 20 - Binding to attributes & CSS - FINI

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,9 +8,11 @@
       @keyup.esc="clearMessage"
       @keyup.enter="alertMessage"
       v-autofocus
-    />
+      :style="errorStyle" />
 
     <button @click="clearMessage">Clear</button>
+
+    <div> {{ message.length }} </div>
 
     <h5 v-if="message.length" class="border-grey">{{ message }}</h5>
     <h6 v-else>Aucun Message saisi 😟</h6>
@@ -33,6 +35,15 @@ export default {
     messageUppercase() {
       console.log("messageUppercase was fired");
       return this.message.toUpperCase() + " " + this.counter;
+    },
+    // eslint-disable-next-line vue/return-in-computed-property
+    errorStyle() {
+      if (this.message.length > 22) {
+        return {
+          'color' : 'red',
+          'background' : 'pink'          
+        }
+      }
     }
   },
   methods: {
@@ -59,7 +70,15 @@ export default {
 </script>
 
 <style>
-.border-grey {
-  border: 1px solid grey;
-}
+  .border-grey {
+    border: 1px solid grey;
+  }
+  input, button {
+    font-size: 23px;
+  }
+  .error {
+    color: red;
+    background: pink;
+  }
+
 </style>
