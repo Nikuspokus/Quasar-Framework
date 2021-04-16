519906361cdbf479ff1b28b014d0c3cde8e3024c
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-04-16 11:59:27 +0200

    Chap 26 - Remove item - FINI

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,11 @@
 <template>
   <q-page padding>
     <ul>
-      <li v-for="task in tasks" :key="task">{{ task }}</li>
+      <li v-for="(task, index) in tasks" :key="task">
+        <div>{{ task.name }} {{ index }}</div>
+        <small>{{ task.date }} @ {{ task.heure }}</small>
+        <button @click="deleteTask(index)">X</button>
+      </li>
     </ul>
   </q-page>
 </template>
@@ -10,7 +14,28 @@
 export default {
   data() {
     return {
-      tasks: ['Aller au magasin', 'Acheter des bannanes', ' acheter despommes']
+      tasks: [
+        {
+          name: "Aller au magasin",
+          date: "16/04/2021",
+          heure: "18:30"
+        },
+        {
+          name: "Acheter des bannanes",
+          date: "18/04/2021",
+          heure: "09:30"
+        },
+        {
+          name: "acheter des pommes",
+          date: "17/04/2021",
+          heure: "09:31"
+        }
+      ]
+    };
+  },
+  methods: {
+    deleteTask(index) {
+      this.tasks.splice(index, 1);
     }
   }
 };
