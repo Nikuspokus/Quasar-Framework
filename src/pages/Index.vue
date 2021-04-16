4eba547a53083240b83215575b93846230414732
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-04-16 11:43:56 +0200

    Chap 25 - Displaying list with v-for - FINI

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,26 +1,8 @@
 <template>
   <q-page padding>
-    <button style="position: absolute; right: 10px" @click="counter++">
-      {{ counter }}
-    </button>
-    <input
-      v-model="message"
-      @keyup.esc="clearMessage"
-      @keyup.enter="alertMessage"
-      v-autofocus
-      :class="{ 'error' : message.length > 22 }"
-      ref="messageInput" />
-
-    <button @click="clearMessage">Clear</button>
-
-    <div> {{ message.length }} </div>
-
-    <h5 v-if="message.length" class="border-grey">{{ message }}</h5>
-    <h6 v-else>Aucun Message saisi 😟</h6>
-    <hr />
-
-    <p>Uppercase message: {{ messageUppercase }}</p>
-    <p>Lowercase message: {{ message | messageLowercase }}</p>
+    <ul>
+      <li v-for="task in tasks" :key="task">{{ task }}</li>
+    </ul>
   </q-page>
 </template>
 
@@ -28,62 +10,10 @@
 export default {
   data() {
     return {
-      message: "I love Vue.JS so hard!",
-      counter: 0
-    };
-  },
-  computed: {
-    messageUppercase() {
-      console.log("messageUppercase was fired");
-      return this.message.toUpperCase() + " " + this.counter;
-    },
-    // eslint-disable-next-line vue/return-in-computed-property
-    errorStyle() {
-      if (this.message.length > 22) {
-        return {
-          'color' : 'red',
-          'background' : 'pink'          
-        }
-      }
+      tasks: ['Aller au magasin', 'Acheter des bannanes', ' acheter despommes']
     }
-  },
-  methods: {
-    clearMessage() {
-      this.message = "";
-    },
-    alertMessage() {
-      alert(this.message);
-    }
-  },
-  filters: {
-    messageLowercase(value) {
-      return value.toLowerCase()
-    }
-  },
-  directives: {
-    autofocus: {
-      inserted(el) {
-        el.focus()
-      }
-    }
-  },
-  mounted() {
-    console.log( this.$refs )
-    this.$refs.messageInput.className = ' bg-green '
-  },
+  }
 };
 </script>
 
-<style>
-  .border-grey {
-    border: 1px solid grey;
-  }
-  input, button {
-    font-size: 23px;
-  }
-  .error {
-    color: red;
-    background: pink;
-  }
-
-</style>
+<style></style>
