4ef8c5fb84f6ed883cc5ad11991255ccfa2d7498
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-04-19 11:13:32 +0200

    Chap 37 - Adding some style to the layout

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,21 +1,10 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh lpR fFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
-        <q-toolbar-title>
-          Quasar App
+        <q-toolbar-title class="absolute-center">
+          Awesome Todo
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -34,15 +23,23 @@
     <q-drawer
       v-model="leftDrawerOpen"
       :breakpoint="767"
+      :width="250"
       show-if-above
       bordered
-      content-class="bg-grey-1"
+      content-class="bg-primary"
     >
-      <q-list>
-        <q-item-label header class="text-grey-8">
+      <q-list dark>
+        <q-item-label header>
           Navigation
         </q-item-label>
-        <q-item v-for="nav in navs" :key="nav.key" :to="nav.to" exact clickable>
+        <q-item
+          v-for="nav in navs"
+          :key="nav.key"
+          :to="nav.to"
+          class="text-grey-4"
+          exact
+          clickable
+        >
           <q-item-section avatar>
             <q-icon :name="nav.icon" />
           </q-item-section>
@@ -95,5 +92,8 @@ export default {
     .q-footer {
       display: none;
     }
-}
+  }
+  .q-drawer .q-router-link--exact-active {
+    color: white !important;
+  }
 </style>
